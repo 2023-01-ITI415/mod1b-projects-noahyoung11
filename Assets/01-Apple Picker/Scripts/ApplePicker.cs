bc5e70b81ee4bb9d358f64db3279212e39889542
Nerr4life <nerr.on.pc@gmail.com>
2023-01-31 23:52:34 -0500

    Created HighScore and ScoreCounter texts, added notifications to ApplePicker when Apple is dropped, Destroy basket on collision with apple

--- Assets/01-Apple Picker/Scripts/ApplePicker.cs
+++ Assets/01-Apple Picker/Scripts/ApplePicker.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ApplePicker : MonoBehaviour
 {
@@ -9,16 +10,30 @@ public class ApplePicker : MonoBehaviour
     public int numBaskets = 3;
     public float basketBottomY = -14f;
     public float basketSpacingY = 2f;
+    public List<GameObject> basketList;
     
     // Start is called before the first frame update
     void Start()
     {
+        basketList = new List<GameObject>();
         for (int i = 0; i < numBaskets; i++){
             GameObject tBasketGO = Instantiate(basketPrefab) as GameObject;
             Vector3 pos = Vector3.zero;
             pos.y = basketBottomY + (basketSpacingY * 1);
             tBasketGO.transform.position = pos;
+            basketList.Add(tBasketGO);
         }
+    }
+
+    public void AppleDestroyed(){
+        GameObject[] tAppleArray = GameObject.FindGameObjectsWithTag("Apple");
+        foreach(GameObject tGO in tAppleArray){
+            Destroy(tGO);
+        }
+        int basketIndex = basketList.Count-1;
+        GameObject tBasketGO = basketList[basketIndex];
+        basketList.RemoveAt(basketIndex);
+        Destroy(tBasketGO);
     }
 
     // Update is called once per frame
