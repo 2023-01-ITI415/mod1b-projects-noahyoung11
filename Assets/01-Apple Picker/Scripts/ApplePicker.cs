dfcd316db01b37357fd16f890e9d0bb11378176d
noahyoung11 <123130749+noahyoung11@users.noreply.github.com>
2023-02-02 20:20:27 -0500

    Update ApplePicker.cs

--- Assets/01-Apple Picker/Scripts/ApplePicker.cs
+++ Assets/01-Apple Picker/Scripts/ApplePicker.cs
@@ -19,7 +19,7 @@ public class ApplePicker : MonoBehaviour
         for (int i = 0; i < numBaskets; i++){
             GameObject tBasketGO = Instantiate(basketPrefab) as GameObject;
             Vector3 pos = Vector3.zero;
-            pos.y = basketBottomY + (basketSpacingY * 1);
+            pos.y = basketBottomY + (basketSpacingY * i);
             tBasketGO.transform.position = pos;
             basketList.Add(tBasketGO);
         }
